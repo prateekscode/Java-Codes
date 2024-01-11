c2181f2ab6750fd7eff35f9a7c17a2d9210701ee
Prateek Gupta <prateekgupta2002@gmail.com>
2024-01-11 18:17:38 +0530

    dyanmic web project

---
+++ WebProject-1/src/main/java/com/india/servlets/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.india.servlets;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("uname");
+		String password = req.getParameter("pwd");
+		System.out.println("Given username and password: " + name + "\t" + password);
+		String msg = "";
+		if (name.equals(password)) {
+			msg = "<h1>Login Succesful<br/>Welcome. Thanks for using me.";
+		} else {
+			msg = "<h1>Login Failed.</h1>";
+		}
+
+		Writer out = resp.getWriter();
+		out.write(msg);
+	}
+
+}
