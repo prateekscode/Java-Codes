4835b1722568221ed8c509bc7e06fdcb8b3f3e71
Prateek Gupta <97242129+prateekscode@users.noreply.github.com>
2024-01-28 13:27:36 +0530

    Delete mvc directory

--- mvc/dao/RegisterDao.java
+++
@@ -1,7 +0,0 @@
-package com.mvc.dao;
-
-import com.mvc.dto.RegisterDTO;
-
-public interface RegisterDao {
-	public void registerUser(RegisterDTO registerDTO);
-}
--- mvc/service/RegisterService.java
+++
@@ -1,8 +0,0 @@
-package com.mvc.service;
-
-import com.mvc.dto.RegisterDTO;
-
-public interface RegisterService{
-
-	public void registerUser(RegisterDTO registerDTO);
-}
